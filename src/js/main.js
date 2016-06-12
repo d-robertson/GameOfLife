d027e6e4b105d83f52d213648d28cbb68d184669
darkenvy <darkenvy6@gmail.com>
2016-06-11 20:14:41 -0700

    Made Responsive and added CSS Styles

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,16 @@
+var gridSize = 60;
+var generation = 0;
+
 function Board() {
   this.grid = [];
+  this.size = gridSize;
 }
 
 Board.prototype.initEmpty = function() {
   this.grid = [];
-  for (var y=0; y<60; y++) {
+  for (var y=0; y<this.size; y++) {
     this.grid.push([]);
-    for (var x=0; x<60; x++) {
+    for (var x=0; x<this.size; x++) {
       this.grid[y].push(false); // Using boolians because its only 1 bit of data
     }
   }
@@ -16,12 +20,12 @@ Board.prototype.commit = function() {
   var html = "";
 
   // Generate HTML data based on array data
-  for (var y=0; y<60; y++) {
-    for (var x=0; x<60; x++) {
+  for (var y=0; y<this.size; y++) {
+    for (var x=0; x<this.size; x++) {
       if (this.grid[x][y]) {
-        html += '<div id="' + ((60*y) + x) + '" class="y"></div>';
+        html += '<div id="' + ((this.size*y) + x) + '" class="y"></div>';
       } else {
-        html += '<div id="' + ((60*y) + x) + '"></div>';
+        html += '<div id="' + ((this.size*y) + x) + '"></div>';
       }
     }
   }
@@ -32,8 +36,8 @@ Board.prototype.commit = function() {
 
 
 Board.prototype.applyRules = function(liveBoard) {
-  for (var y=0; y<60; y++) { 
-    for (var x=0; x<60; x++) {
+  for (var y=0; y<this.size; y++) { 
+    for (var x=0; x<this.size; x++) {
       // Iterate through each cell and apply the 4 rules to it.
       var livingNeighbors = 0;
       for (var i=0; i<9; i++) {
@@ -43,17 +47,15 @@ Board.prototype.applyRules = function(liveBoard) {
         xCoord = (x-1) + (i % 3);
         yCoord = (y-1) + Math.floor(i/3);
 
-        // Out of array bounds error fix. Skip the loops that are OOB
-        // if (xCoord < 0 || xCoord > 59) { continue;}
-        // if (yCoord < 0 || yCoord > 59) { continue;}
+        // Instead of out of bounds errors, correct it by looping
         if (xCoord < 0) { 
-          xCoord = 59
-        } else if (xCoord > 59) {
+          xCoord = this.size -1
+        } else if (xCoord > this.size -1) {
           xCoord = 0;
         }
         if (yCoord < 0) { 
-          yCoord = 59;
-        } else if (yCoord > 59) {
+          yCoord = this.size -1;
+        } else if (yCoord > this.size -1) {
           yCoord = 0;
         }
 
@@ -61,30 +63,23 @@ Board.prototype.applyRules = function(liveBoard) {
         if (liveBoard.grid[ xCoord ][ yCoord ]) {
           livingNeighbors += 1;
         }
-
       }
 
       // Rule 1 - Any live cell with fewer than two live neighbours dies
       if (liveBoard.grid[x][y] && livingNeighbors < 2) {
         this.grid[x][y] = false;
-        // console.log("Rule 1");
-        // console.log("this.grid[x][y] is: ", this.grid[x][y], liveBoard.grid[x][y]);
       }
       // Rule 2 - Any live cell with more than three live neighbours dies
       if (liveBoard.grid[x][y] && livingNeighbors > 3) {
         this.grid[x][y] = false;
-        // console.log("Rule 2");
       }
       // Rule 3 - Any live cell with two or three live neighbours lives, unchanged, to the next generation.
       if (livingNeighbors === 2 || livingNeighbors === 3 ) {
         this.grid[x][y] = liveBoard.grid[x][y]
-        // console.log("Rule 3");
-        // continue;
       }
       // Rule 4 - Any dead cell with exactly three live neighbours will come to life.
       if (liveBoard.grid[x][y] === false && livingNeighbors === 3) {
         this.grid[x][y] = true;
-        // console.log("Rule 4");
       }
     }
   }
@@ -96,33 +91,73 @@ function nextGeneration(live, buffer) {
   buffer.applyRules(live);
   buffer.commit();
   live.grid = buffer.grid;
+  generation += 1;
+  document.title = generation + " generations";
 }
 
+function setBrowserSize(live, buffer) {
+  var startingGridSize = gridSize;
+
+  if ($(window).width() > 768) {
+    gridSize = 60;
+  }
+  if ($(window).width() > 660 && $(window).width() <= 768) {
+    gridSize = 32;
+  }
+  if ($(window).width() > 480 && $(window).width() <= 660) {
+    gridSize = 15;
+  }
+  if ($(window).width() <= 480) {
+    gridSize = 10;
+  }
+
+  // Reset everything due to screen size change
+  if (startingGridSize !== gridSize) { 
+    live.size = gridSize;
+    buffer.size = gridSize;
+    live.grid = [];
+    buffer.grid = [];
+    live.initEmpty();
+    buffer.initEmpty();
+    buffer.commit();
+    startingGridSize = gridSize;
+  }
+}
 
 // On Doc Ready
 $(document).ready(function() {
   var playing = null;
 
+  // Board initialization. Two boards: live & buffer. Buffer is the next generation
   var live = new Board();
   live.initEmpty();
-  
   var buffer = new Board();
-  buffer.grid = live.grid;
+  buffer.initEmpty();
   buffer.commit();
+
+  setBrowserSize(live, buffer); // Set initial screen size
   
+  // Set screen size due to resize
+  $(window).resize(function() {
+    setBrowserSize(live, buffer);
+  })
+
   // Event Deligation
   $('.primary').on("click", "div", function(event) {
     if ($(this).hasClass('y')) {
       $(this).removeClass('y'); // Remove class and remove from grid
-      live.grid[$(this).attr('id') % 60][Math.floor($(this).attr('id') / 60)] = false;
+      live.grid[$(this).attr('id') % gridSize][Math.floor($(this).attr('id') / gridSize)] = false;
     } else {
       $(this).addClass('y'); // Add class and add to grid
-      live.grid[$(this).attr('id') % 60][Math.floor($(this).attr('id') / 60)] = true;
+      console.log($(this).attr('id'));
+      live.grid[$(this).attr('id') % gridSize][Math.floor($(this).attr('id') / gridSize)] = true;
     }  
   })
+  // Next Button
   $('#next').click(function() {
     nextGeneration(live, buffer);
   })
+  // Play Button
   $('#play').click(function() {
     // If not playing, play. Else stop the playing
     if (playing === null) {
